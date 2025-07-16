8f2e85d9ebca9868d58043f2199357c6283c9ee0
aaronbudig <aaronbudig@bymason.com>
2025-07-16 09:14:43 -0700

    events.py 2

--- droidrun/agent/planner/events.py
+++ droidrun/agent/planner/events.py
@@ -1,8 +1,16 @@
 from typing import List
-from llama_index.core.llms import ChatMessage
-from llama_index.core.workflow import Event
-from llama_index.core.tools import FunctionTool
 
+try:
+    from llama_index.core.llms import ChatMessage
+    from llama_index.core.workflow import Event
+except ModuleNotFoundError:  # pragma: no cover
+    class _Stub:
+        def __getattr__(self, name):
+            raise ModuleNotFoundError("llama_index is not installed") from None
+        def __call__(self, *args, **kwargs):
+            raise ModuleNotFoundError("llama_index is not installed") from None
+    ChatMessage = _Stub()
+    Event = _Stub
 
 class InputEvent(Event):
     input: list[ChatMessage]
